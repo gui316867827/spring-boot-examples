f03454daeefbd71db8d22e5de841807a29a0c893
gui316867827 <316867827@qq.com>
2018-11-17 21:57:02 +0800

    redis and jpa

--- spring-boot-web/src/main/java/com/neo/config/RedisConfig.java
+++ spring-boot-web/src/main/java/com/neo/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.neo.config;
 
 import java.lang.reflect.Method;
+import java.time.Duration;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
@@ -8,7 +9,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -38,13 +41,23 @@ public class RedisConfig extends CachingConfigurerSupport{
         };
     }
 
+//    @SuppressWarnings("rawtypes")
+//    @Bean
+//    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+//        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
+//        //设置缓存过期时间
+//        //rcm.setDefaultExpiration(60);//秒
+//        return rcm;
+//    }
+    
     @SuppressWarnings("rawtypes")
     @Bean
-    public CacheManager cacheManager(RedisTemplate redisTemplate) {
-        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
-        //设置缓存过期时间
-        //rcm.setDefaultExpiration(60);//秒
-        return rcm;
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+    	//设置缓存过期时间
+    	RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+    													  .entryTtl(Duration.ofHours(1));
+    	return RedisCacheManager.builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+        		.cacheDefaults(redisCacheConfiguration).build();
     }
     
     @Bean
